f506be67f172ef22a6cf6f9dd77fc56401b72ca0
Naveen Kumar <naveenjangid178@gmail.com>
2025-10-13 14:02:41 +0530

    meta verification

--- server/src/app.js
+++ server/src/app.js
@@ -18,12 +18,13 @@ seedDefaultAdmin();
 
 app.get('/webhook', (req, res) => {
     const VERIFY_TOKEN = process.env.META_SECRET;
+    console.log(VERIFY_TOKEN)
 
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
-    if (mode && token === VERIFY_TOKEN) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
         console.log("WEBHOOK_VERIFIED");
         res.status(200).send(challenge);
     } else {
