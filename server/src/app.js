13e130476192afb5ccadb64e229d56666c95a9a2
Naveen Kumar <naveenjangid178@gmail.com>
2025-10-18 10:55:55 +0530

    cors origin change

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ import { seedDefaultAdmin } from "./utils/seedAdmin.js"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN.split(','),
     credentials: true
 }))
 
